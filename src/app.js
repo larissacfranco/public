1c2a55b50488d186e4b04887158cf93e7131c8fd
larissacfranco <96203589+larissacfranco@users.noreply.github.com>
2022-01-03 16:38:00 -0300

    Projects CRUD

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const { uuid } = require("uuidv4");
-//const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -30,11 +29,36 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+	const { title, url, techs} = request.body;
+
+	const repository = repositories.findIndex(repository => repository.id === id);
+
+	if(repository < 0 ) {
+    return response.status(400).json({error:  "ID não existe"});
+  }
+
+  const neweRepository = {
+    ...repositories[repository], title, url, techs
+  }
+	
+  repositories[repository] = neweRepository
+
+  return response.json(neweRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+	const { id } = request.params;
+
+	const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+	if (repositoryIndex < 0) {
+		return response.status(400).json({error: 'Repository not found.' })
+	}
+
+	repositories.splice(repositoryIndex, 1);
+
+	return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
